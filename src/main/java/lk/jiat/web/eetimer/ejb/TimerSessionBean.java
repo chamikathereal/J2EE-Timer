23309bee2e18d2fed42eca408d2ca76cded1f240
chamikathereal <118957556+chamikathereal@users.noreply.github.com>
2025-06-15 21:54:36 +0530

    day 01 updates

---
+++ src/main/java/lk/jiat/web/eetimer/ejb/TimerSessionBean.java
@@ -0,0 +1,39 @@
+package lk.jiat.web.eetimer.ejb;
+
+import jakarta.annotation.Resource;
+import jakarta.ejb.*;
+
+import java.util.Collection;
+
+@Stateless
+public class TimerSessionBean {
+
+    @Resource
+    private TimerService timerService;
+
+    public void doTask() {
+        timerService.createTimer(10000,"Clock"); // EJB 3.0/3.1
+        //timerService.createIntervalTimer(1000,5000,new TimerConfig()); // EJB new versions
+
+        TimerConfig timerConfig = new TimerConfig();
+        timerConfig.setInfo("Info");
+
+        timerService.createSingleActionTimer(10000, timerConfig);
+
+        Collection<Timer> allTimers = timerService.getTimers();
+        allTimers.forEach(timer -> {
+
+        });
+    }
+
+    @Timeout
+    public void timeOutTask(Timer timer) {
+        System.out.println("Task timed out." + timer);
+    }
+
+//    public void cancelTimer(){
+//        if(timer != null){
+//            timer.cancel();
+//        }
+//    }
+}
---
+++ src/main/java/lk/jiat/web/eetimer/servlet/Test.java
@@ -0,0 +1,43 @@
+package lk.jiat.web.eetimer.servlet;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.web.eetimer.ejb.TaskSessionBean;
+import lk.jiat.web.eetimer.ejb.TimerSessionBean;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+
+    @EJB
+    TaskSessionBean taskSessionBean;
+
+    @EJB
+    TimerSessionBean timerSessionBean;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        timerSessionBean.doTask();
+
+//        Future<String> doTask = taskSessionBean.doTask();
+//
+//        try {
+//            String s = doTask.get();
+//            response.getWriter().println(s);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        } catch (ExecutionException e) {
+//            throw new RuntimeException(e);
+//        }
+
+
+    }
+}
